cf167cb957f16cc39ac03ee2d4ed5d5b3a65d307
AiRa1909 <safa.rasyidin@thekaustschool.org>
2025-10-01 18:21:33 +0300

    Final Code Draft

--- Flashcards Console Program.py
+++ Flashcards Console Program.py
@@ -1,5 +1,5 @@
 import sqlite3
-import os
+#import the sqlite 3 database.
 
 def setup_database():
     conn = sqlite3.connect("flashcards.db")
@@ -11,24 +11,41 @@ def setup_database():
     )
     ''')
     # making a database table with columns question and answer
+    conn.execute("DELETE FROM cards")
+    # clear all cards from past user sessions so that everyone gets the console program fresh
     conn.commit()
     return conn
 
 def add_cards(conn):
     print("Let's add a card: ")
-    question = input("Type the question here: ")
+    question = input("Type the prompt here: ")
     answer = input("Type the answer here: ")
-    #some sql will come here later
-    print("\n Card added!")
 
+    if question and answer:
+        conn.execute("INSERT INTO cards VALUES (?,?)", (question,answer))
+        # the ? ? are placeholders for question and answer.
+        conn.commit()
+        print("\n Card added!")
+    else:
+        print("Please don't leave your prompt and answer empty!")
+        #foolproofing our code haha so that people won't encounter an error or add something empty
+        add_cards(conn)
 def study_cards(conn):
-    #sql
+    cursor = conn.execute("SELECT question, answer FROM cards")
+    cards = cursor.fetchall()
+    # we want to get ALL the cards from the question and answer columns
+
+    if not cards:
+        print("\n oops! Look like you need to go back and add some cards because we don't have any!")
+        #more foolproofing so that people can't run a study session without cards
+        return
+
     print("/n ----- LET'S STUDY ----- ")
     for question,answer in cards:
-        print(f"Question: {question}")
+        print(f"Prompt: {question}")
         input("(press Enter for answer)")
         print(f"Answer: {answer}")
-        print("\n You got it! That's correct")
+        print("\n Let's study the next; ")
 
 def main():
     conn = setup_database()
@@ -38,7 +55,8 @@ def main():
         print("PRESS 2. Study cards")
         print("PRESS 3. Exit")
 
-        choice = input("Choose: ")
+        choice = input("Choose: ").strip()
+        # strip for precautions and more foolproofing
 
         if choice == "1":
             add_cards(conn)
@@ -48,6 +66,9 @@ def main():
             print("Oh well! Goodbye.")
             conn.close()
             break
+        else:
+            print("Please choose one of the given options!!!")
+        #provide users with the seperate options of what they want to do. This will appear after every option has completed (every function of the three options or closing the connection)
 
 if __name__ == "__main__":
     main()
